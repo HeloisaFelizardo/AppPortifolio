cf0681c8459b27e40255cfd9f51ae0f78a7f3099
Heloisa Felizardo <helocmp@yahoo.com.br>
2022-08-30 12:34:58 -0300

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+import Capa from './assets/capa.png';
+import Foto from './assets/foto.png';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image style = {styles.fotoTopo} source={Capa} />
+      <View style={styles.boxDescricao}>
+        <Image style = {styles.fotoPerfil} source={Foto} />
+        <Text style = {styles.titulo}>Heloisa Felizardo</Text>
+        <Text style = {styles.cargo}>Técnologa em Comunicação para Web pela Universidade Paulista e desenvolvedora Front-end, especializada em HTML, CSS, JavaScript, React, React Native, Git. </Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
+
+  fotoTopo: {
+    flex: 1,
+  },
+
+  boxDescricao: {
+    flex: 1,
+    alignItems: 'center',
+  },
+
+  fotoPerfil: {
+    marginTop:20,
+    marginBottom:20,
+    width: 100,
+    height: 100,
+    borderRadius:50
+  },
+  titulo: {
+    marginBottom:10,
+    fontSize: 22,
+  },
+  cargo: {
+    textAlign: 'center',
+    marginHorizontal: 15,
+    fontSize: 16
+  }
 });
